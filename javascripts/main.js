59988b7d97950ca74d2d8a451639c964394667e3
kaiz <merchant.kaiz@gmail.com>
2020-05-05 00:25:08 -0400

    website recreate v1

---
+++ javascripts/main.js
@@ -0,0 +1,68 @@
+function openModal1() {
+  var modal = document.querySelector(".modal1");
+  var modalBg = document.querySelector(".modalBackground");
+  document.querySelector("body").classList.add("modal-open");
+  modalBg.style.display ="block";
+  modal.style.display="block";
+}
+
+function closeModal1(){
+  var modal = document.querySelector(".modal1");
+  var modalBg = document.querySelector(".modalBackground");
+  document.querySelector("body").classList.remove("modal-open");
+  modalBg.style.display ="none";
+  modal.style.display="none";
+}
+
+function openModal2(e) {
+  var modal = document.querySelector(".modal2");
+  var modalBg = document.querySelector(".modalBackground");
+  document.querySelector("body").classList.add("modal-open");
+  modalBg.style.display ="block";
+  modal.style.display="block";
+  e.preventDefault();
+
+}
+
+function closeModal2(){
+  var modal = document.querySelector(".modal2");
+  var modalBg = document.querySelector(".modalBackground");
+  document.querySelector("body").classList.remove("modal-open");
+  modalBg.style.display ="none";
+  modal.style.display="none";
+}
+
+function openModal3(e) {
+  var modal = document.querySelector(".modal3");
+  var modalBg = document.querySelector(".modalBackground");
+  document.querySelector("body").classList.add("modal-open");
+  modalBg.style.display ="block";
+  modal.style.display="block";
+  e.preventDefault();
+
+}
+
+function closeModal3(){
+  var modal = document.querySelector(".modal3");
+  var modalBg = document.querySelector(".modalBackground");
+  document.querySelector("body").classList.remove("modal-open");
+  modalBg.style.display ="none";
+  modal.style.display="none";
+}
+
+function openModal4(e) {
+  var modal = document.querySelector(".modal4");
+  var modalBg = document.querySelector(".modalBackground");
+  document.querySelector("body").classList.add("modal-open");
+  modalBg.style.display ="block";
+  modal.style.display="block";
+  e.preventDefault();
+}
+
+function closeModal4(){
+  var modal = document.querySelector(".modal4");
+  var modalBg = document.querySelector(".modalBackground");
+  document.querySelector("body").classList.remove("modal-open");
+  modalBg.style.display ="none";
+  modal.style.display="none";
+}
